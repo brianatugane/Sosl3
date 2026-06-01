c2cc670ce0f66d2bf51db5291ef3fd7d261e33bb
brianatugane <brianatugane@gmail.com>
2026-06-01 17:07:42 +0200

    Dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,16 +9,16 @@
     <li><router-link to="/about" class="hover:text-yellow-400">{{ $i18n.t('nav.about') }}</router-link></li>
 
 
-    <li class="relative group">
-    <a href="#" class="hover:text-yellow-300">{{ $i18n.t('nav.gallery') }}<span>▼</span></a>
-    <ul class="invisible group-hover:visible opacity-100 absolute bg-white text-green-900 rounded shadow w-48 top-full left-0 z-50">
+    <li class="relative">
+    <a href="#" @click.prevent="toggleDropdown('gallery')" class="hover:text-yellow-300">{{ $i18n.t('nav.gallery') }}<span>▼</span></a>
+    <ul v-show="openDropdown === 'gallery'" class="absolute bg-white text-green-900 rounded shadow w-48 top-full left-0 z-50">
      <li><a href="/photos" class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.photos') }}</a></li>
      <li><a href="/videos" class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.videos') }}</a></li>
     </ul>
 </li>
-  <li class="relative group">
-    <a href="#" class="hover:text-yellow-300">{{ $i18n.t('nav.destination') }}<span>▼</span></a>
-<ul class="invisible group-hover:visible opacity-100 absolute bg-white text-green-900 rounded shadow w-48 top-full left-0 z-50">
+  <li class="relative">
+    <a href="#" @click.prevent="toggleDropdown('destination')" class="hover:text-yellow-300">{{ $i18n.t('nav.destination') }}<span>▼</span></a>
+<ul v-show="openDropdown === 'destination'" class="absolute bg-white text-green-900 rounded shadow w-48 top-full left-0 z-50">
         
         <li><a href="/northern"class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.northern') }}</a></li>
      <li><a href="/southern" class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.southern') }}</a></li>
@@ -32,12 +32,12 @@
     <li><router-link to="/contact" class="hover:text-yellow-400">{{ $i18n.t('nav.contact') }}</router-link></li>
 
     <!-- Language Switcher -->
-    <li class="relative group">
-    <a href="#" class="hover:text-yellow-300 flex items-center gap-1">
+    <li class="relative">
+    <a href="#" @click.prevent="toggleDropdown('language')" class="hover:text-yellow-300 flex items-center gap-1">
       <span>{{ $i18n.t('nav.language') }}</span>
       <span class="text-xs">▼</span>
     </a>
-    <ul class="invisible group-hover:visible opacity-100 absolute bg-white text-green-900 rounded shadow w-32 top-full right-0 z-50">
+    <ul v-show="openDropdown === 'language'" class="absolute bg-white text-green-900 rounded shadow w-32 top-full right-0 z-50">
       <li>
         <button 
           @click="changeLanguage('en')" 
@@ -80,11 +80,18 @@
     </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useI18n } from '../i18n.js'
 
 const { setLocale } = useI18n()
+const openDropdown = ref(null)
+
+const toggleDropdown = (dropdown) => {
+  openDropdown.value = openDropdown.value === dropdown ? null : dropdown
+}
 
 const changeLanguage = (locale) => {
   setLocale(locale)
+  openDropdown.value = null
 }
 </script>
