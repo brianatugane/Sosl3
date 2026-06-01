b2dcbd0b2c59c4797d13f140134e2dc6fe0817e4
brianatugane <brianatugane@gmail.com>
2026-06-01 11:17:12 +0200

    NavBar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,14 +10,14 @@
 
 
     <li class="relative group">
-    <a href="#" class="hover:text-yellow-300">{{ $i18n.t('nav.gallery') }}<span></span></a>
+    <a href="#" class="hover:text-yellow-300">{{ $i18n.t('nav.gallery') }}<span>▼</span></a>
     <ul class="invisible group-hover:visible opacity-100 absolute bg-white text-green-900 rounded shadow w-48 top-full left-0 z-50">
      <li><a href="/photos" class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.photos') }}</a></li>
      <li><a href="/videos" class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.videos') }}</a></li>
     </ul>
 </li>
   <li class="relative group">
-    <a href="#" class="hover:text-yellow-300">{{ $i18n.t('nav.destination') }}<span></span></a>
+    <a href="#" class="hover:text-yellow-300">{{ $i18n.t('nav.destination') }}<span>▼</span></a>
 <ul class="invisible group-hover:visible opacity-100 absolute bg-white text-green-900 rounded shadow w-48 top-full left-0 z-50">
         
         <li><a href="/northern"class="block px-4 py-2 hover:bg-green-200 text-green-900">{{ $i18n.t('nav.northern') }}</a></li>
